b36f5d57ca352719ad6dca55fe637ffa75ad1ba0
yangyuxin <yangyuxin@jtkjbike.com>
2020-02-08 17:45:22 +0800

    init project

---
+++ grpc-server/src/main/java/com/yyx/grpc/server/GrpcServerApplication.java
@@ -0,0 +1,12 @@
+package com.yyx.grpc.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcServerApplication.class, args);
+    }
+}
---
+++ src/main/java/com/yyx/grpc/envoy/GrpcEnvoyApplication.java
@@ -0,0 +1,11 @@
+package com.yyx.grpc.envoy;
+
+import org.springframework.boot.SpringApplication;
+
+class GrpcEnvoyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcEnvoyApplication.class, args);
+	}
+
+}
